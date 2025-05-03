4196617f69148fde43c087c02d21ce2e19242d3a
gtm001 <gtm.yup@gmail.com>
2025-05-03 16:24:24 +0530

    uploaded

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,31 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <span>{{ __('Welcome to Your Dashboard') }}</span>
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <button type="submit" class="btn btn-sm btn-danger">
+                            {{ __('Logout') }}
+                        </button>
+                    </form>
+                </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success alert-dismissible fade show" role="alert">
+                            {{ session('status') }}
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                    @endif
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
-    </div>
+                    <p class="mb-0">{{ __('You are successfully logged in to your account!') }}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @endsection
